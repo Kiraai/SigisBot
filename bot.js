48395cb3215466e22cb0043db4ae48f1502090d1
Kiraai <35786313+Kiraai@users.noreply.github.com>
2018-01-28 21:31:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,6 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs")
 const config = require("./config.json");
-const args = message.content.slice(prefix.length).trim().split(/ +/g);
-const command = args.shift().toLowerCase();
 
 client.on("message", (message) => {
   
