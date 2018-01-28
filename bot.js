b1e958a3db733c65120ff8d82706c0e881bcb4a7
Kiraai <35786313+Kiraai@users.noreply.github.com>
2018-01-28 19:24:12 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,27 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
-// Set the prefix
+const args = message.content.slice(prefix.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase();
 let prefix = "&";
+
+
 client.on("message", (message) => {
   // Exit and stop if the prefix is not there or if user is a bot
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  if (message.content.startsWith(prefix + "ping")) {
+  if (command === "ping") {
     message.channel.send("pong!");
-  } else
-  if (message.content.startsWith(prefix + "foo")) {
+  }
+  if (command === "foo") {
     message.channel.send("bar!");
+  }
+  if (command === "build") {
+    let klasa = args[0];
+    if (klasa === warrior) {
+    message.channel.send("Hey, &{message.author.username}, these are following builds for warrior:\nBannerslave Berserker: https://qtfy.eu/guildwars/warrior/bannerslave-berserker/ \nwubba lubba dub dub);
+    }
   }
 });
 
-// THIS  MUST  BE  THIS  WAY
+// THIS  MUST  BE  THIS  WAY, pls no touchy-touchy
 client.login(process.env.BOT_TOKEN);
