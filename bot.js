b616c00bf0ed9d5837d0e32157801a026a9fe395
Kiraai <35786313+Kiraai@users.noreply.github.com>
2018-01-28 20:04:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,26 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const fs = require("fs")
 const config = require("./config.json");
 
-// Set the prefix
-let prefix = "!";
 client.on("message", (message) => {
   // Exit and stop if the prefix is not there or if user is a bot
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-  if (message.content.startsWith(prefix + "ping")) {
+  if(message.content.startsWith(config.prefix + "prefix")) {
+  // Gets the prefix from the command (eg. "!prefix +" it will take the "+" from it)
+  let newPrefix = message.content.split(" ").slice(1, 2)[0];
+  // change the configuration in memory
+  config.prefix = newPrefix;
+
+  // Now we have to save the file.
+  fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
+}
+   
+  if (message.content.startsWith(config.prefix + "ping")) {
     message.channel.send("pong!");
   } else
-  if (message.content.startsWith(prefix + "foo")) {
+  if (message.content.startsWith(config.prefix + "foo")) {
     message.channel.send("bar!");
   }
 });
