2f4515f3821c0e950354aebc8adf5329c658a394
Kiraai <35786313+Kiraai@users.noreply.github.com>
2018-01-28 19:34:40 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const args = message.content.slice(prefix.length).trim().split(/ +/g);
 const command = args.shift().toLowerCase();
+
 let prefix = "&";
 
 
@@ -9,13 +10,12 @@ client.on("message", (message) => {
   // Exit and stop if the prefix is not there or if user is a bot
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  if (command === "ping") {
-    message.channel.send("pong!");
-  }
-  
-  if (command === "foo") {
-    message.channel.send("bar!");
-  }
+ if(command === 'ping') {
+  message.channel.send('Pong!');
+} else
+if (command === 'blah') {
+  message.channel.send('Meh.');
+}
 });
 
 // THIS  MUST  BE  THIS  WAY, pls no touchy-touchy
