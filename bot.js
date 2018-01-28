5d0c72dfda58e33574d69ce4a846137ac248e090
Kiraai <35786313+Kiraai@users.noreply.github.com>
2018-01-28 19:24:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ client.on("message", (message) => {
   if (command === "build") {
     let klasa = args[0];
     if (klasa === warrior) {
-    message.channel.send("Hey, &{message.author.username}, these are following builds for warrior:\nBannerslave Berserker: https://qtfy.eu/guildwars/warrior/bannerslave-berserker/ \nwubba lubba dub dub);
+    message.channel.send("Hey, &{message.author.username}, these are following builds for warrior:\nBannerslave Berserker: https://qtfy.eu/guildwars/warrior/bannerslave-berserker/ \nwubba lubba dub dub");
     }
   }
 });
